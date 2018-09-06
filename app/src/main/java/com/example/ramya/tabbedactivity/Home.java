b466d89ccf89110b911fa92d52931649d72c64f5
ramya <ramyaramya2611@gmail.com>
2018-09-06 22:06:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ramya/tabbedactivity/Home.java
@@ -0,0 +1,22 @@
+package com.example.ramya.tabbedactivity;
+
+/**
+ * Created by ramya on 6/9/18.
+ */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class Home extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.home, container, false);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/ramya/tabbedactivity/My_Account.java
@@ -0,0 +1,22 @@
+package com.example.ramya.tabbedactivity;
+
+/**
+ * Created by ramya on 6/9/18.
+ */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class My_Account extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_main, container, false);
+
+        return rootView;
+    }
+}
